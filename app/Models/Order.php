6c50c7cb0a5239d850c1c108a35804689dd8bb02
Zain-Irfan009 <95672563+Zain-Irfan009@users.noreply.github.com>
2022-03-10 11:11:01 +0500

    Update Order.php

--- app/Models/Order.php
+++ app/Models/Order.php
@@ -8,4 +8,17 @@ use Illuminate\Database\Eloquent\Model;
 class Order extends Model
 {
     use HasFactory;
+
+
+    public function has_shop(){
+        return $this->belongsTo('App\Models\User', 'shop_id');
+    }
+
+    public function has_requests(){
+        return $this->hasMany('App\Models\Request');
+    }
+    public function getOrderNameAttribute($value)
+    {
+        return str_replace('#',$this->prefix,$value);
+    }
 }
