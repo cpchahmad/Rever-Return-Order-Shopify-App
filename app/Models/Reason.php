89ccfbdfa5bf8aa65b5254944158952663a2625e
Zain-Irfan009 <95672563+Zain-Irfan009@users.noreply.github.com>
2022-03-10 11:12:55 +0500

    Update Reason.php

--- app/Models/Reason.php
+++ app/Models/Reason.php
@@ -8,7 +8,15 @@ use Illuminate\Database\Eloquent\Model;
 class Reason extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'name', 'category_id', 'shop_id', 'reason_type', 'status',
+    ];
 
+
+    public function has_skus(){
+
+        return $this->hasMany('App\Models\RequestProducts','reason','name');
+    }
 
     public function category()
     {
