81906144dee3aedc1e5016f149c3e36b0038ff1f
Zain-Irfan009 <95672563+Zain-Irfan009@users.noreply.github.com>
2022-03-29 11:50:20 +0500

    commit

--- app/Http/Middleware/VerifyCsrfToken.php
+++ app/Http/Middleware/VerifyCsrfToken.php
@@ -12,6 +12,6 @@ class VerifyCsrfToken extends Middleware
      * @var array<int, string>
      */
     protected $except = [
-        '/email/flow/update','/customer/variant/stock','/customer/order/*','/easypost/webhook','/*'
+        '/email/flow/update','/customer/variant/stock','/customer/order/*','/easypost/webhook','*'
     ];
 }
